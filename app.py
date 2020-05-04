8e8df78f0004d7c8169149e77d0ed742807182c0
eugenlee <42861174+eugenlee@users.noreply.github.com>
2020-05-03 19:09:10 -0700

    auth, unittest

--- app.py
+++ app.py
@@ -2,94 +2,71 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import Player, Game
-# from auth import AuthError, requires_auth
-from models import db, setup_db
+from models import Player, Game, db, setup_db
+from auth import AuthError, requires_auth
 
-app = Flask(__name__)
-setup_db(app)
-CORS(app)
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-# general access
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                            'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                            'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
-@app.route("/players", methods=['GET'])
-# @requires_auth('get:requests')
-def get_players():
-    try:
-        players = Player.query.all()
+    # general access
 
-        return jsonify({
-            'success': True,
-            'players': [player.formatter() for player in players]
-        })
-
-    except:
-        abort(404)
-
-
-@app.route("/games", methods=['GET'])
-# @requires_auth('get:requests')
-def get_games():
-    try:
-        games = Game.query.all()
-        
-        return jsonify({
-            'success': True,
-            'games': [game.formatter() for game in games]
-        })
-
-    except:
-        abort(404)
-
-
-# players with authorization
-
-@app.route("/players", methods=['POST'])
-# @requires_auth('post:requests')
-def add_player():
-
-    body = request.get_json()
-    if not ('first' in body and 'last' in body and 'team' in body):
-        abort(404)
-
-    first = body.get('first')
-    last = body.get('last')
-    team = body.get('team')
-
-    try:
-        player = Player(first=first, last=last, team=team)
-        player.insert()
-
-        return jsonify({
-            'success': True
-        })
-
-    except:
-        abort(422)
-
-
-@app.route("/players/<id>", methods=['PATCH'])
-# @requires_auth('patch:requests')
-def update_player(id):
-
-    player = Player.query.get(id)
-
-    if player:
+    @app.route("/players", methods=['GET'])
+    @requires_auth('get:requests')
+    def get_players(jwt):
         try:
-            body = request.get_json()
+            players = Player.query.all()
 
-            first = body.get('first')
-            last = body.get('last')
-            team = body.get('team')
+            return jsonify({
+                'success': True,
+                'players': [player.formatter() for player in players]
+            })
 
-            if first:
-                player.first = first
-            if last:
-                player.last = last
-            if team:
-                player.team = team
+        except:
+            abort(404)
 
-            player.update()
+
+    @app.route("/games", methods=['GET'])
+    @requires_auth('get:requests')
+    def get_games(jwt):
+        try:
+            games = Game.query.all()
+            
+            return jsonify({
+                'success': True,
+                'games': [game.formatter() for game in games]
+            })
+
+        except:
+            abort(404)
+
+
+    # players with authorization
+
+    @app.route("/players", methods=['POST'])
+    @requires_auth('post:requests')
+    def add_player(jwt):
+
+        body = request.get_json()
+        if not ('first' in body and 'last' in body and 'team' in body):
+            abort(404)
+
+        first = body.get('first')
+        last = body.get('last')
+        team = body.get('team')
+
+        try:
+            player = Player(first=first, last=last, team=team)
+            player.insert()
 
             return jsonify({
                 'success': True
@@ -97,84 +74,113 @@ def update_player(id):
 
         except:
             abort(422)
-    else:
-        abort(404)
 
 
-@app.route("/players/<id>", methods=['DELETE'])
-# @requires_auth('delete:requests')
-def delete_player(id):
+    @app.route("/players/<id>", methods=['PATCH'])
+    @requires_auth('patch:requests')
+    def update_player(jwt, id):
 
-    player = Player.query.get(id)
+        player = Player.query.get(id)
 
-    if player:
+        if player:
+            try:
+                body = request.get_json()
+
+                first = body.get('first')
+                last = body.get('last')
+                team = body.get('team')
+
+                if first:
+                    player.first = first
+                if last:
+                    player.last = last
+                if team:
+                    player.team = team
+
+                player.update()
+
+                return jsonify({
+                    'success': True
+                })
+
+            except:
+                abort(422)
+        else:
+            abort(404)
+
+
+    @app.route("/players/<id>", methods=['DELETE'])
+    @requires_auth('delete:requests')
+    def delete_player(jwt, id):
+
+        player = Player.query.get(id)
+
+        if player:
+            try:
+                player.delete()
+                return jsonify({
+                    'success': True,
+                    'delete': id
+                })
+
+            except:
+                abort(422)
+        else:
+            abort(404)
+
+
+    # games with authorization
+
+    @app.route("/games", methods=['POST'])
+    @requires_auth('post:requests')
+    def add_game(jwt):
+
+        body = request.get_json()
+
+        if not ('home_away' in body and 'venue' in body):
+            abort(404)
+
+        home_away = body.get('home_away')
+        venue = body.get('venue')
+
         try:
-            player.delete()
+            game = Game(home_away=home_away, venue=venue)
+            game.insert()
+
             return jsonify({
-                'success': True,
-                'delete': id
+                'success': True
             })
 
         except:
             abort(422)
-    else:
-        abort(404)
 
 
-# games with authorization
+    # errorhandler
 
-@app.route("/games", methods=['POST'])
-# @requires_auth('post:requests')
-def add_game():
-
-    body = request.get_json()
-
-    if not ('home_away' in body and 'venue' in body):
-        abort(404)
-
-    home_away = body.get('home_away')
-    venue = body.get('venue')
-
-    try:
-        game = Game(home_away=home_away, venue=venue)
-        game.insert()
-
+    @app.errorhandler(422)
+    def unprocessable(error):
         return jsonify({
-            'success': True
-        })
-
-    except:
-        abort(422)
-
-
-# errorhandler
-
-@app.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({
-        "success": False,
-        "error": 404,
-        "message": "resource not found"
-    }), 404
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
 
 
-# @app.errorhandler(AuthError)
-# def handle_auth_error(ex):
-#     return jsonify({
-#         "success": False,
-#         "error": ex.status_code,
-#         'message': ex.error
-#     }), 401
+    @app.errorhandler(AuthError)
+    def handle_auth_error(ex):
+        return jsonify({
+            "success": False,
+            "error": ex.status_code,
+            'message': ex.error
+        }), 401
 
-
-if __name__ == '__main__':
-    app.run()
+    return app
--- test_app.py
+++ test_app.py
@@ -1,1 +1,181 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+from models import Player, Game, db, setup_db
+from flask import Flask
+from app import create_app
 
+MANAGER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Inh5NHpGUk9sV2dBbmhBaFZEbW92YSJ9.eyJpc3MiOiJodHRwczovL2V1Z2VuZWxlZS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5Yjk0YTI2M2Y4MDAwYzhjMjE1MjdlIiwiYXVkIjoibmJhIiwiaWF0IjoxNTg4NTQ4Nzc2LCJleHAiOjE1ODg2MjA3NzYsImF6cCI6IlM2algzNWxBS0xwVlFMVUFwZ2ZJbUxUdjBwRzBnR3pjIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6cmVxdWVzdHMiLCJnZXQ6cmVxdWVzdHMiLCJwYXRjaDpyZXF1ZXN0cyIsInBvc3Q6cmVxdWVzdHMiXX0.X6Biqawmhwc-QP8CificEEhtqApMZmydA5kkUaNoJHqOMVLaLka2_otYNwKTbPNhLLRaW90H_Iq9nPPCfrOPVbrBgAnyCshe8kL37zwGwlg0_UbjOIK5tL6yatKj3425j0GMm7zme65FVrS444o25k8sMmnYYkVcgEiqIRu9A-WtwRcgair8y0Krt0VCi06g-3X4Xygn8551pLkdx9l1W_CbJjDGBRK0vmcOrSrvpnMKShgH5LKSMQLRLOQgNzpcSU9qOmKfUN4c1OKE-w4cHHljSTY3OUT62qKYH-oR1XbTm-bm6R8eIBVQ3J6yiYUrN80UZBMFf0DLGHjxus6ppg'
+
+class TestCases(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.database_name = "basketball_test"
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        self.client = self.app.test_client()
+        self.headers = {'Content-Type': 'application/json'}
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+
+    def test_401_new_player(self):
+        new_player = {
+            "first": "lebron",
+            "last": "james",
+            "team": "lakers"
+        }
+
+        res = self.client.post('/players', json=new_player)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_new_player(self):
+        new_player = {
+            "first": "lebron",
+            "last": "james",
+            "team": "lakers"
+        }
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.post('/players', json=new_player, headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_401_get_players(self):
+        res = self.client.get('/players')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_get_players(self):
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.get('/players', headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_401_new_game(self):
+        new_game = {
+            "home_away": "home",
+            "venue": "staples",
+        }
+
+        res = self.client.post('/games', json=new_game)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_new_game(self):
+        new_game = {
+            "home_away": "home",
+            "venue": "staples",
+        }
+
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.post('/games', json=new_game, headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_401_get_games(self):
+        res = self.client.get('/games')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_get_games(self):
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.get('/games', headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_401_update_player(self):
+        updated_player = {
+            "first": "anthony",
+            "last": "davis",
+            "team": "lakers"
+        }
+
+        res = self.client.patch('/players/1', json=updated_player)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_update_player(self):
+        player = Player(first="Ryo", last="Tawatari", team="YOKOHAMA B-CORSAIRS")
+        player.insert()
+        player_id = player.id
+
+        updated_player = {
+            "first": "anthony",
+            "last": "davis",
+            "team": "lakers"
+        }
+
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.patch(f'/players/{player_id}', json=updated_player, headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_401_delete_player(self):
+        res = self.client.delete('/players/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 401)
+
+
+    def test_delete_player(self):
+        player = Player(first="danny", last="cook", team="lakers")
+        player.insert()
+        player_id = player.id
+
+        self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
+        res = self.client.delete(f'/players/{player_id}', headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
