54be50a86a32f7b828f50975992c8054897d9919
eugenlee <42861174+eugenlee@users.noreply.github.com>
2020-05-03 21:25:38 -0700

    add fan test

--- test_app.py
+++ test_app.py
@@ -6,8 +6,8 @@ from models import Player, Game, db, setup_db
 from flask import Flask
 from app import create_app
 
-MANAGER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Inh5NHpGUk9sV2dBbmhBaFZEbW92YSJ9.eyJpc3MiOiJodHRwczovL2V1Z2VuZWxlZS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5Yjk0YTI2M2Y4MDAwYzhjMjE1MjdlIiwiYXVkIjoibmJhIiwiaWF0IjoxNTg4NTQ4Nzc2LCJleHAiOjE1ODg2MjA3NzYsImF6cCI6IlM2algzNWxBS0xwVlFMVUFwZ2ZJbUxUdjBwRzBnR3pjIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6cmVxdWVzdHMiLCJnZXQ6cmVxdWVzdHMiLCJwYXRjaDpyZXF1ZXN0cyIsInBvc3Q6cmVxdWVzdHMiXX0.X6Biqawmhwc-QP8CificEEhtqApMZmydA5kkUaNoJHqOMVLaLka2_otYNwKTbPNhLLRaW90H_Iq9nPPCfrOPVbrBgAnyCshe8kL37zwGwlg0_UbjOIK5tL6yatKj3425j0GMm7zme65FVrS444o25k8sMmnYYkVcgEiqIRu9A-WtwRcgair8y0Krt0VCi06g-3X4Xygn8551pLkdx9l1W_CbJjDGBRK0vmcOrSrvpnMKShgH5LKSMQLRLOQgNzpcSU9qOmKfUN4c1OKE-w4cHHljSTY3OUT62qKYH-oR1XbTm-bm6R8eIBVQ3J6yiYUrN80UZBMFf0DLGHjxus6ppg'
-
+MANAGER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Inh5NHpGUk9sV2dBbmhBaFZEbW92YSJ9.eyJpc3MiOiJodHRwczovL2V1Z2VuZWxlZS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5Yjk0YTI2M2Y4MDAwYzhjMjE1MjdlIiwiYXVkIjoibmJhIiwiaWF0IjoxNTg4NTY1ODA1LCJleHAiOjE1ODg2Mzc4MDUsImF6cCI6IlM2algzNWxBS0xwVlFMVUFwZ2ZJbUxUdjBwRzBnR3pjIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6cmVxdWVzdHMiLCJnZXQ6cmVxdWVzdHMiLCJwYXRjaDpyZXF1ZXN0cyIsInBvc3Q6cmVxdWVzdHMiXX0.HYbsU4y_ah2_S6sA585pt--YCJpSBOvpi8hT14ypiYrEPpYQ2IYSOrI09yuwUcTiKwLYHvYLEiMkoO8qUl1dMZ_u73x6qfEBnA2se8zFCnJnOMR9qvrWaA_30116FsykTdkHNbKcCw9IXH7aZwgv79z4Rdl88rW4K2-JdwHpPXISx1i9Yhm3CiKzIxWYsQpwWmThuQGI7nhKJVnTn0grMS0CCaUH6g7FsMQQuk4gyFFYCCpg5B5EZnpuO2Logwd2RKbCsH9R27Ce0FaaKHSC1LgF8bPupayLiwv5p6oWuV_-p6qMuTxx3F-GHVaEasAKRnQ8fcyj_JjeH9t2DI2Y3A'
+FAN_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Inh5NHpGUk9sV2dBbmhBaFZEbW92YSJ9.eyJpc3MiOiJodHRwczovL2V1Z2VuZWxlZS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWVhNjI0YmU1NGIxNGMwYzEyNTllZTEwIiwiYXVkIjoibmJhIiwiaWF0IjoxNTg4NTY2MDQzLCJleHAiOjE1ODg2MzgwNDMsImF6cCI6IlM2algzNWxBS0xwVlFMVUFwZ2ZJbUxUdjBwRzBnR3pjIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJnZXQ6cmVxdWVzdHMiXX0.M4GCl_PxUn8IhSsOsOc38X2sOZ_C__UG6-OQrap5e7P657REMdZ6G3jMTVTvh-DMJ3LN1bch5BfaMyiKHwRkg1Ms9EWgb1UOJ9IQ853RUX1h1Q52aCN28x2qDvvOSWnqlcAzJDLUZ1gAHXo4GnIJCHVJtYo9G8waRs_SV79J-Dt8b9_vq2kO8EJEYSmfFQ4IhQnuzwolFVJR4QQMZ1kGn0eniKakTObUeBoSMRHAHi54uAjrDVNPgrNWozkd38Ru-Ph4uz2qjRG0thCoXoSaYaOcsloiGfywKb-0PFFp_6lP7EYV3xNSW6zm4d5dlaSjo2Lcr0YnPwEjeVTSW1ARMQ'
 
 class TestCases(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -67,7 +67,15 @@ class TestCases(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(res.status_code, 401)
 
-    def test_get_players(self):
+    def test_fan_get_players(self):
+        self.headers.update({'Authorization': 'Bearer ' + FAN_TOKEN})
+        res = self.client.get('/players', headers=self.headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+    def test_manager_get_players(self):
         self.headers.update({'Authorization': 'Bearer ' + MANAGER_TOKEN})
         res = self.client.get('/players', headers=self.headers)
         data = json.loads(res.data)
