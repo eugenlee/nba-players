f453c8af738f53918a2561c1816772598f347d55
eugenlee <42861174+eugenlee@users.noreply.github.com>
2020-05-04 10:33:44 -0700

    auth

--- auth.py
+++ auth.py
@@ -3,12 +3,11 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-import os
 
 
-AUTH0_DOMAIN = os.environ.get("eugenelee.auth0.com")
-ALGORITHMS = os.environ.get('ALGORITHMS')
-API_AUDIENCE = os.environ.get('API_AUDIENCE')
+AUTH0_DOMAIN = 'eugenelee.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'nba'
 
 # AuthError Exception
 
